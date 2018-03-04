d2e9a0f5d5fac5035bd0ad93b5968d2f41b37886
Bernardjacques <bernardjacques789@gmail.com>
2018-03-04 17:09:32 +0100

    clonetime

--- index.php
+++ index.php
@@ -1,68 +1,100 @@
 <?php
-function ecrireJSON($tache, $terminer)
-{
-  $filename = "./todo.json";
-  $tab = array("Nom" => $tache, "Terminer" => $terminer);
-  $json = json_encode($tab);
-  $json .= "\n";
-  $file=file_put_contents($filename, $json, FILE_APPEND | LOCK_EX);
-}
-function afficheJSON($terminer)
-{
-  $filename = "./todo.json";
-  $tab = file($filename);
-  for($i=0; $i < sizeof($tab); $i++)
-  {
-    $obj = json_decode($tab[$i]);
-    $nom = $obj->{'Nom'};
-    if($obj->{'Terminer'} == $terminer)
-     {
-      $txt = '<input type="checkbox" name="tache_ligne[]" value="';
-      $txt .= $i.'" ';
-      $txt .= $terminer?"checked":"";
-      $txt .= ">";
-      $txt .= '<label ';
-      $txt .= 'class="';
-      $txt .= $terminer?"tache_terminer":"tache_non_terminer";
-      $txt .= '" ';
-      $txt .= 'for="'.$nom.'">'.$nom.'</label>';
-      $txt .= "<br/>";
-      echo $txt;
-    }
-  }
-}
-function enregistreJSON($ligne)
-{
+ 
+try {
+    //je me connecte a MySQL
+    $db = new PDO('mysql:host=localhost;dbname=todolist;charset=utf8', 'root', 'user');
+}   
 
-  $ligne = (int)$ligne;
-  $filename = "./todo.json";
-  $tab_file = file($filename);
-  $obj = json_decode($tab_file[$ligne]);
-  $nom = $obj->{'Nom'};
-  $tab = array("Nom" => $nom, "Terminer" => true );
-  $json = json_encode($tab);
-  $json .= "\n";
-  $tab_file[$ligne] = $json;
-  $file=file_put_contents($filename, $tab_file, LOCK_EX);
+catch(Exception $erreur) {
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur: ' .$erreur->getMessage());
+
 }
-$options = array(
-  'tache'     => FILTER_SANITIZE_STRING
-);
-$result = filter_input_array(INPUT_POST, $options);
-if($result != null && $result != FALSE && $_SERVER['REQUEST_METHOD']=='POST'){
-  if(isset($_POST["ajouter"]) && $_POST["ajouter"] == "Ajouter"){
-    $tache=$_POST["tache"];
-    if(!empty($tache)){
-      ecrireJSON($tache, false);
+
+/*FORMULAIRE*/
+  /*Sanitisation*/
+  $options = array(
+    'task' => FILTER_SANITIZE_STRING,
+    'taskligne' => FILTER_SANITIZE_STRING
+  );
+  $result = filter_input_array(INPUT_POST, $options);
+//   /*fin Sanitisation*/
+//   //sqluête POST:
+//   //vérification des valeurs après la Sanitisation
+//   if($result != null && $result != FALSE && $_SERVER['REQUEST_METHOD']=='POST')
+//   {
+
+    if(isset($_POST["task"])){
+
+      $task=$_POST["task"];
+      insertmysql($task, 0);
     }
-  }
-  if(isset($_POST["submit"]) && $_POST["submit"] == "Enregistrer"){
-    $tache_ligne = $_POST["tache_ligne"];
-    for ($i = 0; $i <sizeof($tache_ligne); $i++){
-      enregistreJSON($tache_ligne[$i]);
+
+    // if(isset($_POST["submit"]) || isset($_POST["uncheck"])) {
+
+    //   $task_ligne = $_POST["taskligne"];
+    //   // print_r($task_ligne);
+    //   for($i = 0; $i < sizeof($task_ligne); $i++){
+    //     updatemysql($task_ligne[$i]);
+    //     // enregistreJSON($task_ligne);
+
+    //   } 
+    // }
+
+    if(isset($_POST["Supprimer"])){
+      
+      $task_ligne = $_POST["taskligne"];
+      // print_r($task_ligne);
+      for($i = 0; $i < sizeof($task_ligne); $i++){
+        deletemysql($task_ligne[$i]);
+      }
     }
+    /*nom de la task contenu dans le "TextBox"*/
+    /*$task=$_POST["task"];
+    /utilisation de la fonction ecrireJSON/
+    /ecrireJSON($task, false);*/
+
+
+  function insertmysql($task, $status)
+  {
+      global $db;    
+    $sql = $db->prepare("INSERT INTO `tasks`(`task`, `status`) VALUES (:task,:status)");
+    // $sql = $db->prepare("INSERT INTO tasks(task, status) VALUES(:task, :status)");
+
+    $sql->execute(array(
+        "task" => $task,
+        "status" => $status
+  ));
+    
   }
-}
+
+  function updatemysql($id)
+  {
+    global $db;
+    $reponse = $db->prepare("SELECT * FROM tasks WHERE id = :id LIMIT 0,1"); 
+    $reponse->execute(array(
+        "id" => $id,
+        ));
+
+    $ligne = $reponse->fetch();
+    $status = $ligne["status"] == "true" ? "false": "true";
+
+    $sql = $db->prepare("UPDATE tasks SET status = :status WHERE id = :id");
+    $sql->execute(array(
+        "id" => $id,
+        "status" => $status
+        ));
+  }
+  function deletemysql($id)
+  {
+    global $db;
+    $reponse = $db->prepare("DELETE FROM tasks WHERE id = :id"); 
+    $reponse->execute(array(
+        "id" => $id,
+        ));
+
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -72,32 +104,73 @@ if($result != null && $result != FALSE && $_SERVER['REQUEST_METHOD']=='POST'){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/grundschrift" type="text/css"/> 
     <title>Todo-List</title>
 </head>
 <body>
-    <section class="task_list">
-            <h4>À Faire</h4>
-              <form action="formjson.php" method="POST">
-                <?php
-                  afficheJSON(false);
-                ?>
-                <input type="submit" name="submit" value="Enregistrer">
-              </form>
-            <h4>Archive</h4>
-                <?php
-                  afficheJSON(true);
-                ?>
-    </section>
-
-    <section>
-        <div class="addtask">
-            <h2>Ajouter une Tâche</h2>
-            <form action="formjson.php" method="POST">
-                <label for="task">La tâche à effectuer</label>
-                <input type="text" name="tache" value="">
-                <input type="submit" name="ajouter" value="Ajouter">
-            </form>
+    <h1>TO-DO List</h1>
+    <section class="notepad">
+        <div class="bookmark">
+            <button type="button" class="header--theme-button active" style="--theme-primary:orange; --theme-secondary:white;">
+                </button>
+            <button type="button" class="header--theme-button" style="--theme-primary:#2196F3; --theme-secondary:#eee;">
+                </button>
+            <button type="button" class="header--theme-button" style="--theme-primary:purple; --theme-secondary:white;">
+                </button>
+            <button type="button" class="header--theme-button" style="--theme-primary:#F44336; --theme-secondary:white;">
+                </button>
+            <button type="button" class="header--theme-button" style="--theme-primary:green; --theme-secondary:white;">
+                </button>
+            <button type="button" class="header--theme-button" style="--theme-primary:#FFEB3B; --theme-secondary:#222;">
+                </button>
         </div>
+        <section class="task_list">
+            <div class="todo">
+                <h3>À Faire</h3>
+                <form action="index.php" method="post">
+                    <?php
+                        $result = $db->query('SELECT * FROM tasks WHERE status="0"');
+                        $resultArr = $result->fetchAll(PDO::FETCH_ASSOC);
+                        foreach ($resultArr as $task)
+                        {
+                            print $task["task"] . "-" . $status["0"];
+                            echo "<input type='checkbox' name='tache[]' value='".($donnees['nomtache'])."'/>
+                            <label for='choix'>".($donnees['nomtache'])."</label><br />";
+                        }
+                    ?>
+                    <input class="button" type="submit" name="archiver" value="Fini">
+                    
+                </form>
+            </div>
+            <div class="done">
+                <h3>Archive</h3>
+                <form action="index.php" method="post" name="formchecked">
+                    <?php
+                        $result = $db->query('SELECT * FROM tasks WHERE status="1"');
+                        $resultArr = $result->fetchAll(PDO::FETCH_ASSOC);
+                        foreach ($resultArr as $task)
+                        {
+                            print $task["task"] . "-" . $status["1"];
+                            echo "<input type='checkbox' name='tache[]' value='".($donnees['nomtache'])."'/>
+                            <label for='choix'>".($donnees['nomtache'])."</label><br />";
+                        }
+                    ?>
+                    <input class= "button" type="submit" name="uncheck" value="uncheck">
+                    <input class= "button" type="submit" name="delete" value="delete">
+                </form>
+            </div>
+        </section>
+        <section class="addtask">
+            <div>
+                <h2>Ajouter une Tâche</h2>
+                <form action="index.php" method="POST">
+                <input type="text" name="task" value="">
+                <input class="button" type="submit" name="submit" value="Valider">
+                </form>
+            </div>
+        </section>
     </section>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="theme.js"></script>
 </body>
 </html>
